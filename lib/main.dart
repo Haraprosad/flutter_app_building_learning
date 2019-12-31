90c87ce2ce778df5ec9e4f6ae0c0873639f53fac
Haraprosad Biswas <haraprosadbiswas123@gmail.com>
2019-12-31 14:16:06 +0600

    3)Container widget -(1)

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/screens_for_our_app/first_screen.dart';
+import 'package:flutter_app/screens_for_our_app/home.dart';
 
-void main()=> runApp(MyFlutterApp());
-
-//Class for reducing main method code
-   class MyFlutterApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-
-    return  MaterialApp(title: "Welcome to my app",
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(title:Text("Thanks Boss")),
-            body:   FirstScreen()
-        )
-    );
-  }
-
-   }
+void main(){
+  runApp(
+      MaterialApp(
+        title: "Exploring UI widgets",
+        home: Home(),
+      )
+  );
+}
---
+++ lib/screens_for_our_app/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      color: Colors.deepPurple,
+      child: Text(
+        "Niloy",
+        textDirection: TextDirection.ltr,
+      ),
+    );
+  }
+
+}
