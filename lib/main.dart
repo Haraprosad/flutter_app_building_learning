393ce3f70071e9c5890b28817d908eae1fc3af63
Haraprosad Biswas <haraprosadbiswas123@gmail.com>
2019-12-31 21:34:16 +0600

    11)Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,56 +3,50 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(
     MaterialApp(
-      title: "Long ListView Practice",
+      title: "Statefull widget tutorial",
       home: Scaffold(
-        appBar: AppBar(title: Text("ListView Tutorial")),
-        body: getListView(),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          tooltip: "You pressed floating button.",
-          onPressed: (){
-            debugPrint("You have pressed floatingButton.");
-          },
-        ),
+        appBar: AppBar(title: Text("Stateful widget tutorial"),),
+        body: favouriteCity(),
       ),
     )
   );
 }
 
-//Step-01: make data source
-List<String> listItems(){
-  var list = List<String>.generate(1000, (counter)=>"Item $counter");
-  return list;
+//Step-1: create a class extended StatefulWidget
+
+class favouriteCity extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _favouriteCityState();
+  }
+
 }
 
-//Step-02: convert data source to widget
-Widget getListView(){
-  var litem = listItems();
-  var listV = ListView.builder(itemBuilder: (context,index){
-    return ListTile(
-      title: Text("${litem[index]}"),
-      leading: Icon(Icons.landscape),
-      trailing: Icon(Icons.phone),
-      subtitle: Text("${litem[index]} is displayed"),
-      onTap: (){
-       showSnackBar(context,"${litem[index]}");
-        },
+//Step-02: Create a class extended State<above_class>
+class _favouriteCityState extends State<favouriteCity>{
+  String cityName = "";
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      child: Column(
+        children: <Widget>[
+          TextField(
+            textAlign: TextAlign.center,
+            onSubmitted: (String userInput){
+              //we can also use here onChanged: then the change will be instant
+              //Step-03: call the setState method and put the changing value
+              setState(() {
+                cityName = userInput;
+              });
+            },
+          ),
+          Padding(
+            child: Text("Your city name is: $cityName"),
+            padding: EdgeInsets.only(top: 8),
+          ),
+        ],
+      )
     );
-  });
-  return listV;
-}
-
-//For showing snackbar
-
-void showSnackBar(BuildContext context,String a){
-  var msg = SnackBar(
-    content: Text("You have pressed $a"),
-    action: SnackBarAction(
-      label: "UNDO",
-      onPressed: (){
-        debugPrint("You are pressed dummy Undo.");
-      },
-    ),
-  );
-  Scaffold.of(context).showSnackBar(msg);
+  }
 }
