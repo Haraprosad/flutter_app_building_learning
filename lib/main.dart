671d1ac63fc9aaad61eb883596ae78ffd612e041
Haraprosad Biswas <haraprosadbiswas123@gmail.com>
2019-12-31 14:23:26 +0600

    3)Container widget -(3)

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_app/screens_for_our_app/home.dart';
 void main(){
   runApp(
       MaterialApp(
+        debugShowCheckedModeBanner: false, //To remove debug symbol from screen
         title: "Exploring UI widgets",
         home: Home(),
       )
--- lib/screens_for_our_app/home.dart
+++ lib/screens_for_our_app/home.dart
@@ -3,16 +3,18 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.deepPurple,
-      width: 200.0, //Height and width do not work here
-      height: 100.0,// Container has taken the height and width of
-      child: Text(  // materialApp widget (For Flutter properties)
+    return Center(child:  Container( //To take into account of container height
+      alignment: Alignment.center,  //and weight we can use Center widget
+      color: Colors.deepPurple,    // as the parent widget of container
+      width: 200.0,
+      height: 100.0,
+      child: Text(
         "Niloy",
         textDirection: TextDirection.ltr,
       ),
+    )
     );
+
   }
 
 }
