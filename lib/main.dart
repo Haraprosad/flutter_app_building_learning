db31c5e3435953cdd27178514b4bf5db32b09738
Haraprosad Biswas <haraprosadbiswas123@gmail.com>
2019-12-31 19:17:50 +0600

    9)ListView for many elements

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: "Learning ListView",
-    home: Scaffold(
-      appBar: AppBar(title: Text("ListView Structured")),
-      body: getListView(),
-    ),
-  ));
+void main(){
+  runApp(
+    MaterialApp(
+      title: "Long ListView Practice",
+      home: Scaffold(
+        appBar: AppBar(title: Text("ListView Tutorial")),
+        body: getListView(),
+      ),
+    )
+  );
 }
 
-class getListView extends StatelessWidget {
-  AssetImage assetImage = AssetImage('images/plc.jpg');
+//Step-01: make data source
+List<String> listItems(){
+  var list = List<String>.generate(1000, (counter)=>"Item $counter");
+  return list;
+}
 
-  @override
-  Widget build(BuildContext context) {
-    var listView = ListView(
-      children: <Widget>[
-        ListTile(
-          leading: Icon(Icons.landscape),
-          title: Text("Welcome boss:Niloy"),
-          subtitle: Text("Boss,you have to reach your goal"),
-          trailing: Image(
-            image: assetImage,
-          ),
-        ),
-        ListTile(
-          leading: Icon(Icons.landscape),
-          title: Text("Welcome boss:Niloy"),
-          subtitle: Text("Boss,you have to reach your goal"),
-          trailing: Image(
-            image: assetImage,
-          ),
-        ),
-        ListTile(
-          leading: Icon(Icons.phone),
-          title: Text("Make Phone call"),
-          onTap: () {
-            var alertDialog = AlertDialog(
-              title: Text("Successfull"),
-              content: Text("You have pressed: 'Make a phone call element'"),
-            );
-            showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return alertDialog;
-                });
-          },
-        ),
-        Text(
-          "This is a widget array.",
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 25),
-        ),
-      ],
+//Step-02: convert data source to widget
+Widget getListView(){
+  var litem = listItems();
+  var listV = ListView.builder(itemBuilder: (context,index){
+    return ListTile(
+      title: Text("${litem[index]}"),
+      leading: Icon(Icons.landscape),
+      trailing: Icon(Icons.phone),
+      subtitle: Text("${litem[index]} is displayed"),
+      onTap: (){
+        var alertDialog = AlertDialog(
+          title: Text("You pressed ${litem[index]}"),
+        );
+      showDialog(context: context,builder: (BuildContext context){
+        return alertDialog;
+      });
+        },
     );
-    return listView;
-  }
+  });
+  return listV;
 }
