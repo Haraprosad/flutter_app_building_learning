b9302b073835d25cbbf85b2c92fec576d7992fa9
Haraprosad Biswas <haraprosadbiswas123@gmail.com>
2019-12-31 14:31:32 +0600

    3)Container widget -(4)[Margin+Padding]

--- lib/screens_for_our_app/home.dart
+++ lib/screens_for_our_app/home.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Center(child:  Container( //To take into account of container height
-      alignment: Alignment.center,  //and weight we can use Center widget
-      color: Colors.deepPurple,    // as the parent widget of container
-      width: 200.0,
-      height: 100.0,
+    return Center(child:  Container(
+      alignment: Alignment.center,
+      color: Colors.deepPurple,
+      padding: EdgeInsets.all(16),//To use margin/padding: EdgeInset is used
+      margin: EdgeInsets.only(top: 55,left: 35),
       child: Text(
         "Niloy",
         textDirection: TextDirection.ltr,
