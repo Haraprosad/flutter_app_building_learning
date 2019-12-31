1a88ee6561dbdf384819c02325944771b3fbb697
Haraprosad Biswas <haraprosadbiswas123@gmail.com>
2019-12-31 14:19:15 +0600

    3)Container widget -(2)

--- lib/screens_for_our_app/home.dart
+++ lib/screens_for_our_app/home.dart
@@ -6,7 +6,9 @@ class Home extends StatelessWidget{
     return Container(
       alignment: Alignment.center,
       color: Colors.deepPurple,
-      child: Text(
+      width: 200.0, //Height and width do not work here
+      height: 100.0,// Container has taken the height and width of
+      child: Text(  // materialApp widget (For Flutter properties)
         "Niloy",
         textDirection: TextDirection.ltr,
       ),
