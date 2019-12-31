9afb8f5eed3f3c075737a882153d16a8c3a5d92a
Haraprosad Biswas <haraprosadbiswas123@gmail.com>
2019-12-31 18:46:30 +0600

    8)Simple ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/screens_for_our_app/home.dart';
 
-void main(){
-  runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false, //To remove debug symbol from screen
-        title: "Exploring UI widgets",
-        home: Home(),
-      )
-  );
+void main() {
+  runApp(MaterialApp(
+    title: "Learning ListView",
+    home: Scaffold(
+      appBar: AppBar(title: Text("ListView Structured")),
+      body: getListView(),
+    ),
+  ));
+}
+
+class getListView extends StatelessWidget {
+  AssetImage assetImage = AssetImage('images/plc.jpg');
+
+  @override
+  Widget build(BuildContext context) {
+    var listView = ListView(
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.landscape),
+          title: Text("Welcome boss:Niloy"),
+          subtitle: Text("Boss,you have to reach your goal"),
+          trailing: Image(
+            image: assetImage,
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.landscape),
+          title: Text("Welcome boss:Niloy"),
+          subtitle: Text("Boss,you have to reach your goal"),
+          trailing: Image(
+            image: assetImage,
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.phone),
+          title: Text("Make Phone call"),
+          onTap: () {
+            var alertDialog = AlertDialog(
+              title: Text("Successfull"),
+              content: Text("You have pressed: 'Make a phone call element'"),
+            );
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return alertDialog;
+                });
+          },
+        ),
+        Text(
+          "This is a widget array.",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 25),
+        ),
+      ],
+    );
+    return listView;
+  }
 }
